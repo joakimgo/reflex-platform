d1bcfe0aaf5e5d0b9f1aaeeec935188cd4d112d5
Joakim Goldkuhl <joakim@cogmed.com>
2024-12-17 08:44:44 +0100

    fix double-conversion patch

--- haskell-overlays/text-jsstring-8.10/default.nix
+++ haskell-overlays/text-jsstring-8.10/default.nix
@@ -10,5 +10,12 @@ self: super: {
   attoparsec = self.callCabal2nix "attoparsec" self._dep.attoparsec {};
   buffer-builder = appendPatch super.buffer-builder ./buffer-builder-text-jsstring.patch;
   conduit-extra =  (appendPatch super.conduit-extra ./conduit-extra-text-jsstring.patch);
-  double-conversion = appendPatch super.double-conversion ./double-conversion-Add-text-jsstring.patch;
+  double-conversion = overrideCabal super.double-conversion (drv: {
+    src = fetchFromGitHub {
+      owner = "obsidiansystems";
+      repo = "double-conversion";
+      rev = "0f9ddde468687d25fa6c4c9accb02a034bc2f9c3";
+      sha256 = "0sjljf1sbwalw1zycpjf6bqhljag9i1k77b18b0fd1pzrc29wnks";
+    };
+  });
 }
